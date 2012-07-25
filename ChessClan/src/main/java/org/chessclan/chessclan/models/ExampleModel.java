b981942a4236c01e6a5df5457cdad0372b4e9770
Grzegorz.Duszynski <gfduszynski@gmail.com>
2012-07-26 00:22:38 +0200

    Added CheckStyle 2.9.1 support
    Now integration wont pass unless code  style is correct.

---
+++ ChessClan/src/main/java/org/chessclan/chessclan/models/ExampleModel.java
@@ -0,0 +1,22 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.chessclan.chessclan.models;
+
+/**
+ *
+ * @author Giorgio
+ */
+public class ExampleModel {
+
+    /**
+     * Java doc is required.
+     */
+    public ExampleModel() {
+        while (true) {
+            int i = 1 + 1;
+        }
+    }
+
+}
