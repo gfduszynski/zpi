a4b8e1da40c69e9a27f89f6a3e88104d4d4b4bf5
Daniel Engel <danniel.engel@gmail.com>
2012-09-08 10:24:00 +0200

    Usunalem selenium test bo netbeans nie chciał mi zbudowac projektu pomimo że mielem profil development :( wywalilem niepotrzebne pliki konfiguracyjne, tzn., 2 od fejsów, bo te pliki byly do jsf 1.2, w 2.1 mozna wszystko przez adnotacje robic, lekko zmodyfikowalem weba

--- ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/DashboardBean.java
+++ ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/DashboardBean.java
@@ -5,9 +5,13 @@
 package org.chessclan.presentationTier.frontControllers;
 
 import java.io.Serializable;
+import java.util.ArrayList;
+import java.util.Date;
 import java.util.List;
 import javax.faces.bean.ManagedBean;
+import javax.faces.bean.ManagedProperty;
 import javax.faces.bean.ViewScoped;
+import org.chessclan.businessTier.services.AuthenticationService;
 import org.chessclan.dataTier.models.Club;
 import org.chessclan.dataTier.models.User;
 
@@ -18,13 +22,37 @@ import org.chessclan.dataTier.models.User;
 @ManagedBean
 @ViewScoped
 public class DashboardBean implements Serializable {
-    
+
+    @ManagedProperty(value = "#{authenticationService}")
+    private AuthenticationService authenticationService; // injected Spring defined service for bikes
     private List<User> users;
     private List<Club> clubs;
     private User updatedUser;
     private Club updatedClub;
 
     public DashboardBean() {
+        users = new ArrayList<User>();
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+        users.add(new User(1, "Daniello", "Engello", "daniel.engel@poczta.pl", new Date(), new Date(), "passwd"));
+    }
+
+    public String logout() {
+
+        authenticationService.logout();
+
+        return "/default.xhtml";
     }
 
     public List<User> getUsers() {
@@ -57,5 +85,13 @@ public class DashboardBean implements Serializable {
 
     public void setUpdatedClub(Club updatedClub) {
         this.updatedClub = updatedClub;
+    }
+
+    public AuthenticationService getAuthenticationService() {
+        return authenticationService;
+    }
+
+    public void setAuthenticationService(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
     }
 }
