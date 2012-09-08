9812bde36bc13dbfc3903b579f0432559a49dd10
Daniel Engel <danniel.engel@gmail.com>
2012-09-08 10:24:00 +0200

    Usunalem selenium test bo netbeans nie chciał mi zbudowac projektu pomimo że mielem profil development :( wywalilem niepotrzebne pliki konfiguracyjne, tzn., 2 od fejsów, bo te pliki byly do jsf 1.2, w 2.1 mozna wszystko przez adnotacje robic, lekko zmodyfikowalem weba

--- ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/LoginBean.java
+++ ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/LoginBean.java
@@ -4,58 +4,58 @@ import java.io.Serializable;
 import javax.faces.application.FacesMessage;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.SessionScoped;
+import javax.faces.bean.RequestScoped;
 import javax.faces.context.FacesContext;
 import org.chessclan.businessTier.services.AuthenticationService;
 
 @ManagedBean(name = "loginBean")
-@SessionScoped
+@RequestScoped
 public class LoginBean implements Serializable {
-	private static final long serialVersionUID = 14535324523452345L;
 
-	private String login;
-	private String password;
+    private String login;
+    private String password;
+    @ManagedProperty(value = "#{authenticationService}")
+    private AuthenticationService authenticationService; // injected Spring defined service for bikes
 
-	@ManagedProperty(value = "#{authenticationService}")
-	private AuthenticationService authenticationService; // injected Spring defined service for bikes
+    public String login() {
 
+        boolean success = authenticationService.login(login, password);
 
-	public String login() {
+        if (success) {
+            boolean isAdmin = true;
+            if (isAdmin) {
+                return "/administration/dashboard.xhtml";
+            } else {
+                return "/default.xhtml"; // return to application but being logged now  
+            }
 
-		boolean success = authenticationService.login(login, password);
-		
-		if (success){
-			return "/default.xhtml"; // return to application but being logged now 
-		}
-		else{
-			FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Login or password incorrect."));			
-			return "/authorization/login.xhtml";
-		}
-	}
+        } else {
+            FacesContext.getCurrentInstance().addMessage(null, new FacesMessage("Login or password incorrect."));
+            return "/authorization/login.xhtml";
+        }
+    }
 
-	public String getLogin() {
-		return login;
-	}
+    public String getLogin() {
+        return login;
+    }
 
-	public void setLogin(String login) {
-		this.login = login;
-	}
+    public void setLogin(String login) {
+        this.login = login;
+    }
 
-	public String getPassword() {
-		return password;
-	}
+    public String getPassword() {
+        return password;
+    }
 
-	public void setPassword(String password) {
-		this.password = password;
-	}
+    public void setPassword(String password) {
+        this.password = password;
+    }
 
+    public AuthenticationService getAuthenticationService() {
+        return this.authenticationService;
+    }
 
-
-	public void setAuthenticationService(AuthenticationService authenticationService) {
-		this.authenticationService = authenticationService;
-	}
-
-	
-	
-	
+    public void setAuthenticationService(AuthenticationService authenticationService) {
+        this.authenticationService = authenticationService;
+    }
 }
