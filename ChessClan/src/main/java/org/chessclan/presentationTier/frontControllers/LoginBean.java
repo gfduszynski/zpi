19d064fcab09ff7a8d6607898ecd58ff9998c12e
Daniel Engel <danniel.engel@gmail.com>
2012-10-13 18:46:43 +0200

    login works

--- ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/LoginBean.java
+++ ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/LoginBean.java
@@ -1,44 +1,69 @@
 package org.chessclan.presentationTier.frontControllers;
 
 import java.io.IOException;
+import java.util.Map;
 import javax.faces.bean.ManagedBean;
-import javax.faces.bean.RequestScoped;
+import javax.faces.bean.ManagedProperty;
+import javax.faces.bean.SessionScoped;
 import javax.faces.context.ExternalContext;
 import javax.faces.context.FacesContext;
 import javax.servlet.RequestDispatcher;
 import javax.servlet.ServletException;
 import javax.servlet.ServletRequest;
 import javax.servlet.ServletResponse;
+import org.chessclan.businessTier.businessObjects.UserManagementBO;
+import org.springframework.security.authentication.BadCredentialsException;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.userdetails.User;
 
 /**
  *
  * @author Grzegorz Duszyski <gfduszynski@gmail.com>
  */
-@RequestScoped
+@SessionScoped
 @ManagedBean(name = "loginBean")
 public class LoginBean {
 
     private String username = "";
     private String password = "";
     private boolean loggedIn = false;
+    @ManagedProperty("#{UserManagementBO}")
+    UserManagementBO umBO;
+    private boolean loginError;
+    @ManagedProperty("#{sessionBean}")
+    SessionBean sessionBean;
 
     /**
      * @return @throws IOException
      * @throws ServletException
      */
     public String doLogin() throws IOException, ServletException {
+        this.loginError = false;
+
         ExternalContext context = FacesContext.getCurrentInstance().getExternalContext();
 
         RequestDispatcher dispatcher =
-                ( (ServletRequest) context.getRequest() ).getRequestDispatcher("/j_spring_security_check");
+                ((ServletRequest) context.getRequest()).getRequestDispatcher("/j_spring_security_check");
 
         dispatcher.forward((ServletRequest) context.getRequest(),
                 (ServletResponse) context.getResponse());
 
         FacesContext.getCurrentInstance().responseComplete();
 
-        //TODO checking if registration completed or user not found
-        
+        Map sessionMap = FacesContext.getCurrentInstance().getExternalContext().getSessionMap();
+
+        if (((BadCredentialsException) sessionMap.get("SPRING_SECURITY_LAST_EXCEPTION")) != null) {
+            this.loginError = true;
+        } else {
+            Authentication auth = umBO.getLoggedUserAuthentication();
+            if (auth != null) {
+                sessionBean.setUser(umBO.findUserByEmail(((User)auth.getPrincipal()).getUsername()));
+                sessionBean.setLoggedUser(true);
+            } else {
+                this.loginError = true;
+            }
+        }
+
         // Faces are going to exit, sot it'sok to return null
         return null;
     }
@@ -84,4 +109,30 @@ public class LoginBean {
     public void setLoggedIn(final boolean loggedIn) {
         this.loggedIn = loggedIn;
     }
+
+    public UserManagementBO getUmBO() {
+        return umBO;
+    }
+
+    public void setUmBO(UserManagementBO umBO) {
+        this.umBO = umBO;
+    }
+
+    public boolean isLoginError() {
+        return loginError;
+    }
+
+    public void setLoginError(boolean loginError) {
+        this.loginError = loginError;
+    }
+
+    public SessionBean getSessionBean() {
+        return sessionBean;
+    }
+
+    public void setSessionBean(SessionBean sessionBean) {
+        this.sessionBean = sessionBean;
+    }
+    
+    
 }
--- ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/RegistrationBean.java
+++ ChessClan/src/main/java/org/chessclan/presentationTier/frontControllers/RegistrationBean.java
@@ -4,12 +4,14 @@
  */
 package org.chessclan.presentationTier.frontControllers;
 
+import java.io.IOException;
 import java.util.Date;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 import javax.faces.bean.ManagedBean;
 import javax.faces.bean.ManagedProperty;
-import javax.faces.bean.ViewScoped;
+import javax.faces.bean.SessionScoped;
+import javax.servlet.ServletException;
 import org.chessclan.businessTier.businessObjects.ClubBO;
 import org.chessclan.businessTier.businessObjects.UserManagementBO;
 import org.chessclan.dataTier.models.User;
@@ -19,7 +21,7 @@ import org.chessclan.dataTier.models.User;
  * @author Daniel
  */
 @ManagedBean(name = "rgsBean")
-@ViewScoped
+@SessionScoped
 public class RegistrationBean {
 
     private String firstName;
@@ -51,6 +53,8 @@ public class RegistrationBean {
     ClubBO clubBO;
     @ManagedProperty("#{sessionBean}")
     SessionBean sessionBean;
+    @ManagedProperty("#{loginBean}")
+    LoginBean loginBean;
 
     public RegistrationBean() {
         regOption = 0;
@@ -63,7 +67,7 @@ public class RegistrationBean {
         this.regError = false;
         this.statuteError = false;
         pattern = Pattern.compile(EMAIL_PATTERN);
-        this.sex=0;
+        this.sex = 0;
     }
 
     public boolean validateFirstName() {
@@ -182,13 +186,12 @@ public class RegistrationBean {
             User u = umBO.registerUser(email, email, true, password, null, null, null, 0);
             u = umBO.assignRole(u.getUserId(), "ROLE_CLUB");
             sessionBean.setUser(u);
-        }
-        else{
+        } else {
             regError = true;
         }
     }
 
-    public void register() {
+    public void register() throws IOException, ServletException {
 
         boolean val1 = validateFirstName();
         boolean val2 = validateLastName();
@@ -200,8 +203,10 @@ public class RegistrationBean {
         if (val1 && val2 && val3 && val4 && val5) {
             User u = umBO.registerUser(email, email, true, password, firstName, lastName, birthDate, sex);
             u = umBO.assignRole(u.getUserId(), "ROLE_USER");
-            sessionBean.setUser(u);
 
+            loginBean.setUsername(u.getEmail());
+            loginBean.setPassword(u.getPassword());
+            loginBean.doLogin();
         } else {
             this.regError = true;
         }
@@ -399,4 +404,14 @@ public class RegistrationBean {
     public void setUmBO(UserManagementBO umBO) {
         this.umBO = umBO;
     }
+
+    public LoginBean getLoginBean() {
+        return loginBean;
+    }
+
+    public void setLoginBean(LoginBean loginBean) {
+        this.loginBean = loginBean;
+    }
+    
+    
 }
