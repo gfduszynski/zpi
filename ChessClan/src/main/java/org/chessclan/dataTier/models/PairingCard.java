cf9816a2e4764a090c26f55aa9a830f2e9d4b4b1
Grzegorz Duszyński <gfduszynski@gmail.com>
2012-10-15 13:49:41 +0200

    [S02] DB Refactoring, TurnamentBO '2

---
+++ ChessClan/src/main/java/org/chessclan/dataTier/models/PairingCard.java
@@ -0,0 +1,131 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package org.chessclan.dataTier.models;
+
+import java.io.Serializable;
+import javax.persistence.Basic;
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import javax.persistence.NamedQueries;
+import javax.persistence.NamedQuery;
+import javax.persistence.Table;
+import javax.validation.constraints.NotNull;
+
+/**
+ *
+ * @author Giorgio
+ */
+@Entity
+@Table(name = "pairing_cards")
+@NamedQueries({
+    @NamedQuery(name = "PairingCard.findAll", query = "SELECT p FROM PairingCard p")})
+public class PairingCard implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Basic(optional = false)
+    @Column(name = "id")
+    private Integer id;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "player")
+    private int player;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "opponent")
+    private int opponent;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "tournament")
+    private int tournament;
+    @Basic(optional = false)
+    @NotNull
+    @Column(name = "score")
+    private float score;
+
+    public PairingCard() {
+    }
+
+    public PairingCard(Integer id) {
+        this.id = id;
+    }
+
+    public PairingCard(Integer id, int player, int opponent, int tournament, float score) {
+        this.id = id;
+        this.player = player;
+        this.opponent = opponent;
+        this.tournament = tournament;
+        this.score = score;
+    }
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public int getPlayer() {
+        return player;
+    }
+
+    public void setPlayer(int player) {
+        this.player = player;
+    }
+
+    public int getOpponent() {
+        return opponent;
+    }
+
+    public void setOpponent(int opponent) {
+        this.opponent = opponent;
+    }
+
+    public int getTournament() {
+        return tournament;
+    }
+
+    public void setTournament(int tournament) {
+        this.tournament = tournament;
+    }
+
+    public float getScore() {
+        return score;
+    }
+
+    public void setScore(float score) {
+        this.score = score;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof PairingCard)) {
+            return false;
+        }
+        PairingCard other = (PairingCard) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "org.chessclan.dataTier.models.PairingCard[ id=" + id + " ]";
+    }
+    
+}
