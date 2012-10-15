a3d3b355b6a2f5457b338d7b9edf60b3c1cf5017
Grzegorz Duszyński <gfduszynski@gmail.com>
2012-10-15 14:01:56 +0200

    [S02] Merging with Tomek '2

--- ChessClan/src/main/java/org/chessclan/businessTier/businessObjects/TournamentBO.java
+++ ChessClan/src/main/java/org/chessclan/businessTier/businessObjects/TournamentBO.java
@@ -5,20 +5,35 @@
 package org.chessclan.businessTier.businessObjects;
 
 import java.io.Serializable;
+import java.util.Date;
+import org.chessclan.dataTier.models.Club;
 import org.chessclan.dataTier.models.Tournament;
+import org.chessclan.dataTier.repositories.PairingCardRepository;
+import org.chessclan.dataTier.repositories.RoundRepository;
 import org.chessclan.dataTier.repositories.TournamentRepository;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
  *
- * @author Xcays
+ * @author Xcays & Grzesiek
  */
 @Service("TournamentBO")
 public class TournamentBO implements Serializable{    
     
     @Autowired
     private TournamentRepository tRepo;
+    
+    @Autowired
+    private RoundRepository rRepo;
+    
+    @Autowired
+    private PairingCardRepository gRepo;
+    
+    public Tournament registerTournament(String tName, Date tDate, String tDesc, Club tClub){
+        Tournament t = new Tournament(null, tName, tDate, tDesc, tClub);
+        return tRepo.saveAndFlush(t);
+    }
     
     public Tournament saveTournament(Tournament t){
         return tRepo.save(t);
